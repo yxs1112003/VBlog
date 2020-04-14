c7abf473e7421167c930444066db24d246549530
yinxuesong <yinxuesong@dankegongyu.com>
2020-04-14 11:42:09 +0800

    tmp commit

---
+++ blogserver/src/main/java/org/sang/mapper/YBInputMapper.java
@@ -0,0 +1,12 @@
+package org.sang.mapper;
+
+import org.sang.bean.Article;
+import org.sang.bean.YBInputEntity;
+
+/**
+ * @author xupeng
+ * @date: 14/04/2020
+ */
+public interface YBInputMapper {
+  int addNewSaleInput(YBInputEntity ybInputEntity);
+}
---
+++ doc/yb_ddl.sql
@@ -0,0 +1,25 @@
+-- 录入销量
+CREATE TABLE `yb_input` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(255) COMMENT '组别',
+  `saler_name` varchar(255) COMMENT '销售姓名',
+  `visit_name` varchar(255) COMMENT '拜访客户(家)',
+  `whole_sale` text  COMMENT '批发',
+  `dominant_position` text  COMMENT '制高点',
+  `display` text COMMENT '陈列',
+  `other` int(11) COMMENT '其他',
+  `visit_by_route` int(11) COMMENT '走线拜访',
+  `visit_by_phone` datetime COMMENT '电话拜访',
+  `order_count` datetime COMMENT '订单(张)',
+  `water_sale_count` int(11)  COMMENT '水销量(箱)',
+  `water_333` int(11) DEFAULT 0 COMMENT '333水',
+  `water_555` int(11) DEFAULT 0 COMMENT '555水',
+  `water_1500` int(11) DEFAULT 0 COMMENT '1.5L水',
+  `water_4500` int(11) DEFAULT 0 COMMENT '4.5L水',
+  `drink_sale_count` int(11) DEFAULT 0 COMMENT '饮料量(箱)',
+  `huojia` int(11) DEFAULT 0 COMMENT '火咖',
+  `mishui` int(11) DEFAULT 0 COMMENT '蜜水',
+  `huojia2` int(11) DEFAULT 0 COMMENT '火咖2',
+  `created_at` datetime COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=123 DEFAULT CHARSET=utf8
